bbeecbbe58218df06fa5dc985d2697ab44389603
a1yama <a1yama1123@gmail.com>
2017-01-18 11:07:20 +0900

    ブログのURLが変わっていたので対応

--- config/blog_config.php
+++ config/blog_config.php
@@ -1,7 +1,7 @@
 <?php
 $blogConfig = [
-    'mona' => ['url' => 'http://www.keyakizaka46.com/mob/news/diarKiji.php?site=k46&cd=member&ct=10', 'text' => 'もなのブログが更新されたよ！'],
-    'beri' => ['url' => 'http://www.keyakizaka46.com/mob/news/diarKiji.php?site=k46&cd=member&ct=20', 'text' => 'ベリのブログが更新されたよ！'],
+    'mona' => ['url' => 'http://www.keyakizaka46.com/s/k46o/diary/member/list?ima=0000&ct=10', 'text' => 'もなのブログが更新されたよ！'],
+    'beri' => ['url' => 'http://www.keyakizaka46.com/s/k46o/diary/member/list?ima=0000&ct=20', 'text' => 'ベリのブログが更新されたよ！'],
 ];
 
 return compact('blogConfig');
